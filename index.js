5bde5f35690f244cae1e3b708080a94ab6320052
nikhil <n1998naik@gmail.com>
2023-01-28 10:54:48 +0530

    Chenge

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ mongoose.connection.on("disconnected", () => {
 
 app.use(express.json());
 
+app.use("/", (req,res) => {
+    res.json({message : "Hello from express app"})
+})
+
 app.use("/api/city", cityRoute);
 app.use("/api/cars", carsRoute);
 
